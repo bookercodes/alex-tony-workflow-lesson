9d4cd84a28a7fd9494e2103c4a7a53efa953e748
Alex Booker <booker@booker.codes>
2025-08-21 12:39:09 +0100

    dump

---
+++ src/mastra/index.ts
@@ -0,0 +1,20 @@
+
+import { Mastra } from '@mastra/core/mastra';
+import { PinoLogger } from '@mastra/loggers';
+import { LibSQLStore } from '@mastra/libsql';
+import { weatherWorkflow } from './workflows/weather-workflow';
+import { weatherAgent } from './agents/weather-agent';
+import { tbdWorkflow } from './workflows/tbd-workflow';
+
+export const mastra = new Mastra({
+  workflows: { weatherWorkflow, tbdWorkflow },
+  agents: { weatherAgent },
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
+  }),
+  logger: new PinoLogger({
+    name: 'Mastra',
+    level: 'info',
+  }),
+});
