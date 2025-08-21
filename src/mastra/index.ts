b4363c85556f1551043ce8f1cf070223d8172b65
Alex Booker <booker@booker.codes>
2025-08-21 14:49:08 +0100

    fix

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,13 +2,10 @@
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
-import { weatherWorkflow } from './workflows/weather-workflow';
-import { weatherAgent } from './agents/weather-agent';
-import { tbdWorkflow } from './workflows/tbd-workflow';
+import { tweetWorkflow } from './workflows/tweet-workflow';
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow, tbdWorkflow },
-  agents: { weatherAgent },
+  workflows: {  tbdWorkflow: tweetWorkflow },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
